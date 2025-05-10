900eb0af312102c2f89cb0b0d011e3b257ec6a4b
Sahan <sahangeesaradev11@proton.me>
2025-05-10 20:09:42 +0530

    change 1

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from fastapi import FastAPI
-from ariadne import gql, make_executable_schema, QueryType
-from ariadne.asgi import GraphQL
-
-type_defs = gql("""
-    type Query {
-        hello: String!
-    }
-""")
-
-query = QueryType()
-
-@query.field("hello")
-def resolve_hello(_, info):
-    return "Hello, world!"
-
-schema = make_executable_schema(type_defs, query)
 
 app = FastAPI()
-app.mount("/graphql", GraphQL(schema, debug=True))
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
